a5c0f545e1343e25d3f67c435f4da5776664d423
Fakhri Maulana Falah <fakhrimf23@gmail.com>
2023-04-03 11:12:35 +0700

    added product and toko controller

--- database/migrations/2023_04_03_033228_create_tokos_table.php
+++ database/migrations/2023_04_03_033228_create_tokos_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('nama');
             $table->string('map');
+            $table->string('icon_url'); 
             $table->foreignId('id_user')->constrained('users');
             $table->timestamps();
         });
