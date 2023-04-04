df6dd669e376c699fe3c123d0f42848b0c74ead3
gilangsatya19 <101808832+gilangsatya19@users.noreply.github.com>
2023-04-04 21:11:31 +0700

    update

--- resources/views/layouts/mainHome.blade.php
+++ resources/views/layouts/mainHome.blade.php
@@ -5,6 +5,8 @@
             integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" 
             integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+        
+        <link rel="stylesheet" href="{{asset('css/navbar.css')}}">
         <style>
             .banner{
                 height: 50vh;
@@ -13,105 +15,6 @@
                 background: url('{{asset('coffeeAroundU/assets/halaman_awal/register.png')}}');
                 
             }
-            .nav-atas {
-            background: #fff;
-            width: 100%;
-            padding: 10px 10%;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            position: relative;
-            }
-
-            .logo {
-                width: 90px;
-            }
-
-            .user-pic {
-                width: 40px;
-                border-radius: 50px;
-                cursor: pointer;
-                margin-left: 30px;
-            }
-
-            nav ul {
-                width: 100%;
-                text-align: left;
-            }
-
-            .nav-atas ul li {
-                display: inline-block;
-                list-style: none;
-                margin: 10px 20px;
-            }
-
-            .nav-atas ul li a {
-                color: #000000;
-                text-decoration: none;
-            }
-
-            .sub-menu-wrap {
-                position: absolute;
-                top: 100%;
-                right: 10%;
-                width: 500px;
-                max-height: 0px;
-                overflow: hidden;
-                transition: max-height: 0.5s;
-            }
-            .sub-menu-wrap.open-menu{
-                max-height: 400px;
-            }
-            .sub-menu{
-                border: 2px solid #898686;
-                border-radius: 25px;
-                background: #ffffff;
-                padding: 20px;
-                margin: 10px;
-            }
-            .user-info{
-                display:flex;
-                align-items: center;
-            }
-            .user-info h3{
-                font-weight: 500;
-            }
-            .user-info img{
-                width:60px;
-                border-radius: 50%;
-                margin-right: 15px;
-            }
-            .sub-menu hr{
-                border:0;
-                height:1px;
-                width:100%;
-                background: #ccc;
-                margin: 15px 0 10px;
-            }
-            .sub-menu-link{
-                display: flex;
-                align-items: center;
-                text-decoration: none;
-                color: #525252;
-                margin: 12px 0;
-            }
-            .sub-menu-link p{
-                width:100%;
-            }
-            .sub-menu-link img{
-                width: 40px;
-                background: #e5e5e5;
-                border-radius: 50%;
-                padding: 8px;
-                margin-right: 15px;
-            }
-            .sub-menu-link span{
-                font-size: 22px;
-            }
-            .sub-menu-link span{
-                font-size:16px;
-            }
-            
         </style>
     </head>
     <body>
@@ -119,10 +22,10 @@
             <nav class="nav-atas">
                 <img src="https://cdn.icon-icons.com/icons2/2620/PNG/512/among_us_player_red_icon_156942.png" class="logo">
                 <ul>
-                    <li><a href="#" class="fw-semibold">Home</a></li>
-                    <li><a href="#" class="fw-semibold">About Us</a></li>
-                    <li><a href="#" class="fw-semibold">Menu in Area</a></li>
-                    <li><a href="#" class="fw-semibold">Show My Menu</a></li>
+                    <li><a href="/home/main/home" class="fw-semibold">Home</a></li>
+                    <li><a href="/home/main/about" class="fw-semibold">About Us</a></li>
+                    <li><a href="/home/main/menu-in-area" class="fw-semibold">Menu in Area</a></li>
+                    <li><a href="/home/main/my-menu" class="fw-semibold">Show My Menu</a></li>
                 </ul>
                 <img src="{{asset('coffeeAroundU/assets/navbar/icon_account.png')}}" class="user-pic" onclick="toggleMenu()">
                 <div class="sub-menu-wrap" id="subMenu">
