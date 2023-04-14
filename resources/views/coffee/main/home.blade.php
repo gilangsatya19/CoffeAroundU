5adea7ae5e2129f6f56c0e19f68485eb8fda82ff
Fakhri Maulana Falah <fakhrimf23@gmail.com>
2023-04-14 22:06:23 +0700

    updated home and header

--- resources/views/coffee/main/home.blade.php
+++ resources/views/coffee/main/home.blade.php
@@ -1,66 +1,64 @@
 @extends('layouts.main')
- 
-@section('title', '')
- 
-@section('navbar')
-    @parent
 
-    
+@section('title', '')
+
+@section('navbar')
+@parent
+
+
 @endsection
- 
+
 @section('content')
-    
-    <div class="container">
-        <div class="container-fluid" style="height: 100%;width:100%; background-size: cover;background: url('{{'../../public/Image/'.$toko->icon_url}}')">
-            <div class="fs-1 container col-lg-10 d-flex justify-content-end" style="height:100%;align-items:center">
-                <form action="" method="GET">
-                    <div class="text-center">
-                    </div>
-                </form>
-            </div>
-        </div>
-        <div class="d-flex">
-                <div>
-                    <p class="fw-bold fs-2 text-black my-3 text-center">Start Your Coffee Journey</p>
-                    
-                    <div class="d-flex">
-                        <p class="fw-semibold fs-4">Your Coffee</p>
-                    </div>
-                    
-                    
-                    
-                </div>
-                <a href="/my_products/create" value="tambah" class="btn btn-primary text-white fw-semibold mt-5" style="font-size: 22px;margin-left:40rem;width:13rem;height: 50px;background-color: #4A2C2A">Add Coffee</a>
-        </div>
-        @if (isset($data))
-            
-        <div class="row">
-                @foreach ($data as $item)
-                <div class="column">
-                        <a href="/my_products/{{$item->id}}" class="link-offset-2 link-underline link-underline-opacity-0 text-black">
-                        <div class="card">
-                            <div class="d-flex  mb-4" style="">
-                                <img src="public/Image/{{$item->foto}}" class="logo rounded-top" style="width: 100%;height: 20vh; object-fit: cover; object-position: center;">
-                            </div>
-                            <p class="fs-5 fw-semibold mx-2">{{$item->nama}}</p>
-                            <p class="fs-6 fw-semibold mx-2">RP. {{$item->harga}}</p>
-                            <p class="fs-6 fw-semibold mx-2">{{$item->deskripsi_produk}}</p>
-                            
-                        </div>
-                    </a>
-                </div>
-                
-                
-                @endforeach
-            </div>
-            
-        @endif        
+
+{{-- <div class="container"> --}}
+<div>
+    <div
+        style="width:100%; height:60vh; background: url('{{'../../public/Image/'.$toko->icon_url}}'); background-size: cover; background-repeat: no-repeat; background-position: center;">
     </div>
-    
+</div>
+<div class="container">
+    <div class="d-flex">
+        <div>
+            <p class="fw-bold fs-2 text-black my-3 text-center">Start Your Coffee Journey</p>
+            <div class="d-flex">
+                <p class="fw-semibold fs-4">Your Coffee</p>
+            </div>
+        </div>
+        <a href="/my_products/create" value="tambah" class="btn btn-primary text-white fw-semibold mt-5"
+            style="font-size: 22px;margin-left:40rem;width:13rem;height: 50px;background-color: #4A2C2A">Add Coffee</a>
+    </div>
+    @if (isset($data))
+
+    <div class="row">
+        @foreach ($data as $item)
+        <div class="column">
+            <a href="/my_products/{{$item->id}}"
+                class="link-offset-2 link-underline link-underline-opacity-0 text-black">
+                <div class="card">
+                    <div class="d-flex  mb-4" style="">
+                        <img src="public/Image/{{$item->foto}}" class="logo rounded-top"
+                            style="width: 100%;height: 20vh; object-fit: cover; object-position: center;">
+                    </div>
+                    <p class="fs-5 fw-semibold mx-2">{{$item->nama}}</p>
+                    <p class="fs-6 fw-semibold mx-2">RP. {{$item->harga}}</p>
+                    <p class="fs-6 fw-semibold mx-2">{{$item->deskripsi_produk}}</p>
+
+                </div>
+            </a>
+        </div>
+
+
+        @endforeach
+    </div>
+</div>
+
+@endif
+{{-- </div> --}}
+
 @endsection
 
 @section('footer')
-    @parent
- 
-    
+@parent
+
+
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,10 +1,11 @@
 <head>
     <title>Coffee Around U @yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous">
+    </script>
+
     <link rel="stylesheet" href="{{asset('css/navbar.css')}}">
     <link rel="icon" href="{{asset('coffeeAroundU/assets/logo/coffeebeans.png')}}" type="image/icon type">
 
@@ -15,33 +16,37 @@
     {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> --}}
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link href="{{ asset('/css/transactionInfo.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('/css/transactionInfo.css') }}" rel="stylesheet">
     {{--  --}}
 
 
     <style>
-        .banner{
+        .banner {
             height: 50vh;
+            background: url('{{asset('coffeeAroundU/assets/halaman_awal/banner.jpg')}}');
             background-size: cover;
             background-position: center;
-            background: url('{{asset('coffeeAroundU/assets/halaman_awal/banner.jpg')}}');
-            
-        }
-        /* Float four columns side by side */
-        .column {
-        float: left;
-        width: 25%;
-        padding: 0 10px;
+
         }
 
-        
+        /* Float four columns side by side */
+        .column {
+            float: left;
+            width: 25%;
+            padding: 0 10px;
+        }
+
+        .blur-bgimage {
+            overflow: hidden;
+            filter: blur(5px);
+        }
 
         /* Clear floats after the columns */
         .row:after {
-        content: "";
-        display: table;
-        clear: both;
-        
+            content: "";
+            display: table;
+            clear: both;
+
         }
 
         /* Responsive columns */
@@ -55,15 +60,16 @@
 
         /* Style the counter cards */
         .card {
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-        
-        
-        margin-bottom: 2rem;
-        background-color: #ffffff;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+
+
+            margin-bottom: 2rem;
+            background-color: #ffffff;
         }
 
-        .card:hover{
-            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+        .card:hover {
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
         }
+
     </style>
 </head>
